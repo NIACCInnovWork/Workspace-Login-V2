fa55132f4d95dc49b702db5431b8d803d0c66462
ariesen <anthony.riesen@niacc.edu>
2022-06-15 22:58:11 -0500

    Updated Windows Cont.
    Updates to the main windows four windows (main, sign in, new user, and sign out).  Mostly, these changes are additional content and formatting.  Additionally, buttons were added with "cancel" functionality built out.  Regular "exit" feature disabled for main window with popup explaining added.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # This application is the updated login system for the NIACC Innovation Workspace.
 
 # Import key libraries
+import tkinter.messagebox
 from tkinter import *
 import datetime as dt
 
@@ -13,6 +14,16 @@ from sign_out_window import *
 root = Tk()  # Must come first because it defines the window
 root.title('Innovation Workspace Login')  # Create title of the window
 
+
+def disable_close():
+    tkinter.messagebox.showinfo("Close Disabled", "The regular exit feature has been disabled to prevent the "
+                                                  "application from closing prematurely. \n \n "
+                                                  "Thank you, "
+                                                  "\n Workspace Staff")
+
+
+root.protocol("WM_DELETE_WINDOW", disable_close)
+
 workspaceLogo = PhotoImage(file='Innovation Workspace Logo-Official-Scaled.png')  # Import Logo
 logoLabel = Label(root, image=workspaceLogo)  # Place Logo into a Label
 
@@ -21,16 +32,16 @@ date = dt.datetime.now()
 
 # Creating buttons for the main page
 signInButton = Button(root, text="Sign In", font=("Arial", 14),
-                      height=5, width=20, command=lambda: open_sign_in(root, date))  # Creating Sign In Button
+                      height=3, width=10, command=lambda: open_sign_in(root, date))  # Creating Sign In Button
 newUserButton = Button(root, text="New User", font=("Arial", 14),
-                       height=5, width=20, command=lambda: open_new_user(root, date))  # Creating New User Button
+                       height=3, width=10, command=lambda: open_new_user(root, date))  # Creating New User Button
 signOutButton = Button(root, text="Sign Out", font=("Arial", 14),
-                       height=5, width=20, command=lambda: open_sign_out(root, date))  # Creating Sign Out Button
+                       height=3, width=10, command=lambda: open_sign_out(root, date))  # Creating Sign Out Button
 
 # Placing Objects on Screen
 logoLabel.grid(row=0, column=0, columnspan=3)
-signInButton.grid(row=1, column=0)
-newUserButton.grid(row=1, column=1)
-signOutButton.grid(row=1, column=2)
+signInButton.grid(row=1, column=0, pady=10)
+newUserButton.grid(row=1, column=1, pady=10)
+signOutButton.grid(row=1, column=2, pady=10)
 
 root.mainloop()  # Creating an event loop
--- sign_in_window.py
+++ sign_in_window.py
@@ -8,25 +8,61 @@ from tkinter import *
 
 def open_sign_in(root, date):
     sign_in_window = Toplevel(root)
-    sign_in_window.geometry("750x250")
+    # sign_in_window.geometry("750x300")
     sign_in_window.title("Sign In Window")
 
-    # Crete Objects
-    sign_in_window_title_label = Label(sign_in_window, text="Sign In", font=("Arial", 14), pady=10)
-    users_list_box = Listbox(sign_in_window)
-    user_name_label = Label(sign_in_window, text="User Name", padx=5)
-    user_name_entry = Entry(sign_in_window, state='disabled')
+    # Functions
+    def exit_button():
+        sign_in_window.destroy()
+        sign_in_window.update()
 
-    visit_date_label = Label(sign_in_window, text="Visit DateTime", padx=5)
-    visit_date_entry = Entry(sign_in_window)
+    # Crete Objects
+    sign_in_window_title_label = Label(sign_in_window, text="User Sign In", font=("Arial", 14), pady=5)
+    users_list_box = Listbox(sign_in_window, width=30, height=15)
+
+    user_name_label = Label(sign_in_window, text="User Name:", padx=5)
+    user_name_entry = Entry(sign_in_window, width=30, state="disabled")
+
+    visit_date_label = Label(sign_in_window, text="Visit DateTime:", padx=5)
+    visit_date_entry = Entry(sign_in_window, width=30)
     visit_date_entry.insert(END, date)
+    visit_date_entry.config(state="disabled")
+
+    visitor_type_label = Label(sign_in_window, text="Visitor Type:", padx=5)
+    visitor_type_entry = Entry(sign_in_window, width=30, state="disabled")
+
+    purpose_label = Label(sign_in_window, text="Visit Purpose:", padx=5, justify="left")
+    purpose_prototyping_checkbox = Checkbutton(sign_in_window, text="Prototyping", justify="left")
+    purpose_class_project_checkbox = Checkbutton(sign_in_window, text="Class Project", justify="left")
+    purpose_personal_project_checkbox = Checkbutton(sign_in_window, text="Personal Project", justify="left")
+    purpose_community_build_checkbox = Checkbutton(sign_in_window, text="Community Build", justify="left")
+    purpose_hanging_out_checkbox = Checkbutton(sign_in_window, text="Hanging Out", justify="left")
+    purpose_work_study_checkbox = Checkbutton(sign_in_window, text="Work Study", justify="left")
+
+    submit_sign_in_button = Button(sign_in_window, text="Sign In", width=10)
+    cancel_sign_in_button = Button(sign_in_window, text="Cancel", width=10, command=exit_button)
 
     # Place Objects
-    sign_in_window_title_label.grid(row=0, column=0, columnspan=2)
-    users_list_box.grid(row=1, column=0, rowspan=3)
+    sign_in_window_title_label.grid(row=0, column=0)
+    users_list_box.grid(row=1, column=0, rowspan=8, padx=(10, 10))
 
-    user_name_label.grid(row=1, column=1)
-    user_name_entry.grid(row=1, column=2)
+    user_name_label.grid(row=1, column=1, sticky=E)
+    user_name_entry.grid(row=1, column=2, padx=(5, 20))
 
-    visit_date_label.grid(row=2, column=1)
-    visit_date_entry.grid(row=2, column=2)
+    visit_date_label.grid(row=2, column=1, sticky=E)
+    visit_date_entry.grid(row=2, column=2, padx=(5, 20))
+
+    visitor_type_label.grid(row=3, column=1, sticky=E)
+    visitor_type_entry.grid(row=3, column=2, padx=(5, 20))
+
+    purpose_label.grid(row=4, column=1, sticky=W)
+    purpose_prototyping_checkbox.grid(row=4, column=2, sticky=W, padx=(15, 10))
+    purpose_class_project_checkbox.grid(row=5, column=2, sticky=W, padx=(15, 10))
+    purpose_personal_project_checkbox.grid(row=6, column=2, sticky=W, padx=(15, 10))
+    purpose_community_build_checkbox.grid(row=7, column=2, sticky=W, padx=(15, 10))
+    purpose_hanging_out_checkbox.grid(row=8, column=2, sticky=W, padx=(15, 10))
+    purpose_work_study_checkbox.grid(row=9, column=2, sticky=W, padx=(15, 10))
+
+    submit_sign_in_button.grid(row=8, column=3, padx=10)
+    cancel_sign_in_button.grid(row=9, column=3, padx=10, pady=(0, 10))
+
